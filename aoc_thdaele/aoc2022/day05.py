7760be4df97866a1fe3c5ad01f5e6e456e2d31bc
thdaele <thomas.vandaele2000@gmail.com>
2022-12-05 14:12:33 +0100

    init files day 5

---
+++ aoc_thdaele/aoc2022/day05.py
@@ -0,0 +1,13 @@
+"""
+--- Day 5: Supply Stacks ---
+https://adventofcode.com/2022/day/5
+"""
+from aocd.models import Puzzle
+
+
+def part_a(puzzle: Puzzle):
+    pass
+
+
+def part_b(puzzle: Puzzle):
+    pass
