33c703a9b1efc661f5741bb4c970d3274b7b937e
thdaele <thomas.vandaele2000@gmail.com>
2022-12-05 12:38:46 +0100

    day 4

--- aoc_thdaele/aoc2022/day04.py
+++ aoc_thdaele/aoc2022/day04.py
@@ -6,8 +6,30 @@ from aocd.models import Puzzle
 
 
 def part_a(puzzle: Puzzle):
-    pass
+    score = 0
+    for line in puzzle.input_data.splitlines():
+        elf1, elf2 = line.split(",")
+        l1, r1 = elf1.split("-")
+        l1, r1 = int(l1), int(r1)
+        l2, r2 = elf2.split("-")
+        l2, r2 = int(l2), int(r2)
+        if l2 <= l1 and r1 <= r2:
+            score += 1
+        elif l1 <= l2 and r2 <= r1:
+            score += 1
+    return score
 
 
 def part_b(puzzle: Puzzle):
-    pass
+    score = 0
+    for line in puzzle.input_data.splitlines():
+        elf1, elf2 = line.split(",")
+        l1, r1 = elf1.split("-")
+        l1, r1 = int(l1), int(r1)
+        l2, r2 = elf2.split("-")
+        l2, r2 = int(l2), int(r2)
+        if r2 >= l1 >= l2:
+            score += 1
+        elif r1 >= l2 >= l1:
+            score += 1
+    return score
