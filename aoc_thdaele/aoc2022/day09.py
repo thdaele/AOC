38b597cb3c71d50ac0ca457b20ce59c52556a7c7
thdaele <thomas.vandaele2000@gmail.com>
2022-12-09 10:20:42 +0100

    UNFINISHED PART 2 (moving to laptop)

--- aoc_thdaele/aoc2022/day09.py
+++ aoc_thdaele/aoc2022/day09.py
@@ -14,6 +14,7 @@ def sign_function(x):
 
 
 def part_a(data):
+    return 0
     visited = set()
     hx, hy, tx, ty = 0, 0, 0, 0
     for line in data.splitlines():
@@ -45,5 +46,50 @@ def part_a(data):
     return len(visited)
 
 
+def move_knot(previous, current):
+    px, py = previous
+    cx, cy = current
+
+    dx, dy = cx - px, cy - py
+    if abs(dx) > 1 and abs(dy) > 1:
+        return cx - sign_function(dx), cy - sign_function(dy)
+    elif abs(dx) > 1:
+        return cx - sign_function(dx), cy
+    elif abs(dy) > 1:
+        return cx, cy - sign_function(dy)
+    return cx, cy
+
+
 def part_b(data):
-    pass
+    data = """R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"""
+    visited = set()
+    hx, hy = 0, 0
+    knots = [(0, 0) for _ in range(9)]
+
+    for line in data.split("\n"):
+        direction, distance = line.split()
+        distance = int(distance)
+
+        for i in range(distance):
+            if direction == "R":
+                hx += 1
+            elif direction == "L":
+                hx -= 1
+            elif direction == "U":
+                hy += 1
+            elif direction == "D":
+                hy -= 1
+
+            knots[0] = move_knot((hx, hy), knots[0])
+            for j in range(1, len(knots)):
+                knots[j] = move_knot(knots[j - 1], knots[j])
+            print(knots[8])
+            visited.add(knots[8])
+    return len(visited)
