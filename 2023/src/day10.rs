d3fd0ba21763109b556e6cd27e5f609970821fa3
thdaele <thomas.vandaele2000@gmail.com>
2023-12-10 19:59:32 +0100

    improvements 2023 day 10

--- 2023/src/day10.rs
+++ 2023/src/day10.rs
@@ -3,52 +3,39 @@ use std::ops::Add;
 use aoc_runner_derive::{aoc, aoc_generator};
 use num::abs;
 
-use crate::day10::Tile::{Ground, Pipe, Start};
-
 #[derive(PartialEq, Debug, Copy, Clone)]
 struct Pos {
-    y: i32,
-    x: i32
+    y: usize,
+    x: usize
 }
 
 impl Pos {
-    fn new(y: i32, x: i32) -> Pos {
+    fn new(y: usize, x: usize) -> Pos {
         Pos { y, x }
     }
 }
 
-impl Add for Pos {
+impl Add<(i32, i32)> for Pos {
     type Output = Pos;
 
-    fn add(self, rhs: Pos) -> Self::Output {
-        Pos::new(self.y + rhs.y, self.x + rhs.x)
+    fn add(self, rhs: (i32, i32)) -> Self::Output {
+        Pos::new((self.y as i32 + rhs.0) as usize, (self.x as i32 + rhs.1) as usize)
     }
 }
 
 #[derive(PartialEq, Copy, Clone)]
-enum Tile {
-    Pipe(Pos, Pos, Pos, bool),
-    Start(Pos, Option<Pos>, Option<Pos>),
-    Ground(Pos)
+struct Tile {
+    pos: Pos,
+    neighbours: Option<(Pos, Pos)>,
+    corner: bool
 }
 
 impl Tile {
-    fn connections(&self) -> Option<(Pos, Pos)> {
-        match self {
-            Ground(_) => None,
-            Pipe(_, neighbour1, neighbour2, _) => Some((*neighbour1, *neighbour2)),
-            Start(_, neighbour1, neighbour2) => Some((neighbour1.unwrap(), neighbour2.unwrap()))
-        }
-    }
-
     fn next(self, last: Pos) -> Pos {
-        let (neighbour1, neighbour2) = match self {
-            Pipe(_, neighbour1, neighbour2, _) => (neighbour1, neighbour2),
-            Start(_, neighbour1, neighbour2) => (neighbour1.unwrap(), neighbour2.unwrap()),
-            _ => unreachable!()
-        };
+        let (neighbour1, neighbour2) = self.neighbours.unwrap();
         if !(neighbour1 == last || neighbour2 == last) {
-            unreachable!()
+            // n1 links to n2 but n2 doesn't link back to n1
+            panic!("{:?} links to {:?}, but {:?} doesn't link back to {:?}", last, self.pos, self.pos, last)
         }
         if neighbour1 == last {
             neighbour2
@@ -58,51 +45,42 @@ impl Tile {
     }
 
     fn connect_start(&mut self, map: &[Vec<Tile>]) {
-        if let Start(pos, neighbour1, neighbour2) = self {
-            // Start not connected yet
-            assert_eq!(*neighbour1, None, "Start is already connected");
-            assert_eq!(*neighbour2, None, "Start is already connected");
-            for dir in [Pos::new(-1, 0), Pos::new(0, 1), Pos::new(1, 0), Pos::new(0, -1)] {
-                let neighbour_pos = *pos + dir;
-                if !bound_check(map, neighbour_pos) {
-                    continue;
-                }
-                let neighbour = get_tile(map, neighbour_pos);
-                let connections = neighbour.connections();
-                if connections.is_none() {
-                    // Neighbour is no pipe
-                    continue;
-                }
-                let connections = connections.unwrap();
-                // If links back to start
-                if connections.0 == *pos || connections.1 == *pos {
-                    if neighbour1.is_none() {
-                        *neighbour1 = Some(neighbour_pos);
-                    } else if neighbour2.is_none() {
-                        *neighbour2 = Some(neighbour_pos);
-                    } else {
-                        unreachable!()
-                    }
-                }
+        // Start not connected yet
+        assert_eq!(self.neighbours, None, "Start is already connected");
+        let mut start_connections = Vec::with_capacity(4); // 4 Directions so max 4 items inserted
+        for dir in [(-1, 0), (0, 1), (1, 0), (0, -1)] {
+            let neighbour_pos = self.pos + dir;
+            if !bound_check(map, neighbour_pos) {
+                continue;
             }
-            if neighbour1.is_none() || neighbour2.is_none() {
-                unreachable!()
+            let neighbour = get_tile(map, neighbour_pos);
+            if neighbour.neighbours.is_none() {
+                // Neighbour is no pipe
+                continue;
             }
-        } else {
-            unreachable!()
+            let connections = neighbour.neighbours.unwrap();
+            // If links back to start
+            if connections.0 == self.pos || connections.1 == self.pos {
+                start_connections.push(neighbour_pos);
+            }
         }
+        assert_eq!(start_connections.len(), 2, "Too much connections on start");
+        let neighbour1 = start_connections.first().unwrap();
+        let neighbour2 = start_connections.last().unwrap();
+        self.neighbours = Some((*neighbour1, *neighbour2));
+        self.corner = !(neighbour1.x == neighbour2.x || neighbour1.y == neighbour2.y);
     }
 }
 
 #[inline]
 fn get_tile(map: &[Vec<Tile>], pos: Pos) -> &Tile {
-    map.get(pos.y as usize).unwrap().get(pos.x as usize).unwrap()
+    map.get(pos.y - 1).unwrap().get(pos.x - 1).unwrap()
 }
 
 #[inline]
 fn bound_check(map: &[Vec<Tile>], pos: Pos) -> bool {
-    let y_range = 0..map.len() as i32;
-    let x_range = 0..map.first().unwrap().len() as i32;
+    let y_range = 1..=map.len();
+    let x_range = 1..=map.first().unwrap().len();
 
     x_range.contains(&pos.x) && y_range.contains(&pos.y)
 }
@@ -113,22 +91,26 @@ fn parse(input: &str) -> (Vec<Vec<Tile>>, Pos) {
     let mut start_pos = None;
     let mut result = vec![];
     for (y, line) in input.lines().enumerate() {
+        // Make y bigger so we can check neighbours and don't have to worry for underflow on usize
+        let y_increased = y + 1;
         let mut grid_line = vec![];
         for (x, char) in line.chars().enumerate() {
-            let pos = Pos::new(y as i32, x as i32);
-            let north = Pos::new(y as i32 - 1, x as i32);
-            let east = Pos::new(y as i32, x as i32 + 1);
-            let south = Pos::new(y as i32 + 1, x as i32);
-            let west = Pos::new(y as i32, x as i32 - 1);
+            // Make y bigger so we can check neighbours and don't have to worry for underflow on usize
+            let x_increased = x + 1;
+            let pos = Pos::new(y_increased, x_increased);
+            let north = Pos::new(y_increased - 1, x_increased);
+            let east = Pos::new(y_increased, x_increased + 1);
+            let south = Pos::new(y_increased + 1, x_increased);
+            let west = Pos::new(y_increased, x_increased - 1);
             let tile = match char {
-                '|' => Pipe(pos, north, south, false),
-                '-' => Pipe(pos, east, west, false),
-                'L' => Pipe(pos, north, east, true),
-                'J' => Pipe(pos, north, west, true),
-                '7' => Pipe(pos, south, west, true),
-                'F' => Pipe(pos, south, east, true),
-                '.' => Ground(pos),
-                'S' => Start(pos, None, None),
+                '|' => Tile{pos, neighbours: Some((north, south)), corner: false},
+                '-' => Tile{pos, neighbours: Some((east, west)), corner: false},
+                'L' => Tile{pos, neighbours: Some((north, east)), corner: true},
+                'J' => Tile{pos, neighbours: Some((north, west)), corner: true},
+                '7' => Tile{pos, neighbours: Some((south, west)), corner: true},
+                'F' => Tile{pos, neighbours: Some((south, east)), corner: true},
+                '.' => Tile{pos, neighbours: None, corner: false},
+                'S' => Tile{pos, neighbours: None, corner: false},
                 _ => unreachable!()
             };
             if char == 'S' {
@@ -148,13 +130,13 @@ fn parse(input: &str) -> (Vec<Vec<Tile>>, Pos) {
 fn part1(input: &(Vec<Vec<Tile>>, Pos)) -> u32 {
     let (map_input, start) = input;
     let map = &mut map_input.clone();
-    let start_tile = map.get_mut(start.y as usize).unwrap().get_mut(start.x as usize).unwrap();
+    let start_tile = map.get_mut(start.y - 1).unwrap().get_mut(start.x - 1).unwrap();
     start_tile.connect_start(map_input);
 
     let start_tile = get_tile(map, *start);
 
     let mut lasts = (*start, *start);
-    let mut nexts = start_tile.connections().unwrap();
+    let mut nexts = start_tile.neighbours.unwrap();
     let mut result = 1;
     // Check if we close the loop
     while nexts.0 != nexts.1 {
@@ -174,38 +156,51 @@ fn part1(input: &(Vec<Vec<Tile>>, Pos)) -> u32 {
 fn part2(input: &(Vec<Vec<Tile>>, Pos)) -> u32 {
     let (map_input, start) = input;
     let map = &mut map_input.clone();
-    let start_tile = map.get_mut(start.y as usize).unwrap().get_mut(start.x as usize).unwrap();
+    let start_tile = map.get_mut(start.y - 1).unwrap().get_mut(start.x - 1).unwrap();
     start_tile.connect_start(map_input);
 
     let start_tile = get_tile(map, *start);
 
     let mut lasts = (*start, *start);
-    let mut nexts = start_tile.connections().unwrap();
+    let mut nexts = start_tile.neighbours.unwrap();
     let mut vertices = vec![*start];
+    let mut boundary_length = 1;
     // Check if we close the loop
     while nexts.0 != nexts.1 {
-        vertices.insert(0, nexts.0);
-        vertices.push(nexts.1);
-
         // find next
         let pos = nexts;
         let tile0 = get_tile(map, pos.0);
         let tile1 = get_tile(map, pos.1);
+
+        // Only the corners are needed to calculate the area with Shoelace formula
+        if tile0.corner {
+            vertices.insert(0, nexts.0);
+        }
+        if tile1.corner {
+            vertices.push(nexts.1);
+        }
+        boundary_length += 2;
+
         let new_nexts = (tile0.next(lasts.0), tile1.next(lasts.1));
         lasts = nexts;
         nexts = new_nexts;
     }
-    vertices.push(nexts.1);
+    let tile = get_tile(map, nexts.1);
+    if tile.corner {
+        vertices.push(nexts.1);
+    }
+    boundary_length += 1;
 
     let mut area = 0;
     let mut j = vertices.last().unwrap();
     for pos in &vertices {
-        area += (j.x + pos.x) * (j.y - pos.y);
+        area += (j.x + pos.x) as i32 * (j.y as i32 - pos.y as i32);
         j = pos;
     }
     let area = abs(area / 2);
 
-    (area - (vertices.len() as i32 / 2) + 1) as u32
+    // Pick's theorem
+    (area - (boundary_length / 2) + 1) as u32
 }
 
 
