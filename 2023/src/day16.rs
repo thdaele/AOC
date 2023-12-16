ef8d3e35855c1807401ec166aeed3d468944664b
thdaele <thomas.vandaele2000@gmail.com>
2023-12-16 11:35:54 +0100

    optimize 2023 day 16

--- 2023/src/day16.rs
+++ 2023/src/day16.rs
@@ -1,12 +1,13 @@
 use std::collections::VecDeque;
-use aoc_runner_derive::{aoc, aoc_generator};
-use itertools::max;
-use rustc_hash::FxHashSet;
-use rayon::prelude::*;
 
-#[aoc_generator(day16)]
-fn parse(input: &str) -> (Vec<Vec<char>>, (usize, usize)) {
-    let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+use aoc_runner_derive::aoc;
+use itertools::max;
+use rayon::prelude::*;
+use rustc_hash::FxHashSet;
+
+// #[aoc_generator(day16)]
+fn parse(input: &str) -> (Box<[&[u8]]>, (usize, usize)) {
+    let grid: Box<[&[u8]]> = input.lines().map(|line| line.as_bytes()).collect::<Vec<&[u8]>>().into_boxed_slice();
     let y_len = grid.len();
     let x_len = grid.first().unwrap().len();
     (grid, (y_len, x_len))
@@ -22,16 +23,16 @@ enum Direction {
 
 #[derive(Eq, PartialEq, Hash, Copy, Clone)]
 struct Point {
-    y: u32,
-    x: u32
+    y: u8,
+    x: u8
 }
 
 impl Point {
-    fn new(y: u32, x: u32) -> Point {
+    fn new(y: u8, x: u8) -> Point {
         Point{y, x}
     }
 
-    fn update(&self, y_len: u32, x_len: u32, direction: Direction) -> Option<Point> {
+    fn update(&self, y_len: u8, x_len: u8, direction: Direction) -> Option<Point> {
         if (direction == Direction::North && self.y == 0)
             || (direction == Direction::South && self.y == y_len - 1)
             || (direction == Direction::East && self.x == x_len - 1)
@@ -48,7 +49,7 @@ impl Point {
     }
 }
 
-fn solve(grid: &[Vec<char>], start: Point, direction: Direction, size: (usize, usize)) -> usize {
+fn solve(grid: &[&[u8]], start: Point, direction: Direction, size: (usize, usize)) -> usize {
     assert!(!grid.is_empty());
     let (y_len, x_len) = size;
     let mut queue = VecDeque::from([(start, direction)]);
@@ -60,67 +61,54 @@ fn solve(grid: &[Vec<char>], start: Point, direction: Direction, size: (usize, u
 
             let char = grid[point.y as usize][point.x as usize];
             match char {
-                '/' => {
+                b'/' => {
                     let updated_direction = match direction {
                         Direction::North => Direction::East,
                         Direction::South => Direction::West,
                         Direction::East => Direction::North,
                         Direction::West => Direction::South,
                     };
-                    let updated_point = point.update(y_len as u32, x_len as u32, updated_direction);
-                    if let Some(updated_point) = updated_point {
+                    if let Some(updated_point) = point.update(y_len as u8, x_len as u8, updated_direction) {
                         queue.push_back((updated_point, updated_direction));
                     }
                 },
-                '\\' => {
+                b'\\' => {
                     let updated_direction = match direction {
                         Direction::North => Direction::West,
                         Direction::South => Direction::East,
                         Direction::East => Direction::South,
                         Direction::West => Direction::North,
                     };
-                    let updated_point = point.update(y_len as u32, x_len as u32, updated_direction);
-                    if let Some(updated_point) = updated_point {
+                    if let Some(updated_point) = point.update(y_len as u8, x_len as u8, updated_direction) {
                         queue.push_back((updated_point, updated_direction));
                     }
                 },
-                '|' => {
+                b'|' => {
                     if direction == Direction::East || direction == Direction::West {
-                        let updated_point = point.update(y_len as u32, x_len as u32, Direction::North);
-                        if let Some(updated_point) = updated_point {
+                        if let Some(updated_point) = point.update(y_len as u8, x_len as u8, Direction::North) {
                             queue.push_back((updated_point, Direction::North));
                         }
-                        let updated_point = point.update(y_len as u32, x_len as u32, Direction::South);
-                        if let Some(updated_point) = updated_point {
+                        if let Some(updated_point) = point.update(y_len as u8, x_len as u8, Direction::South) {
                             queue.push_back((updated_point, Direction::South));
                         }
-                    } else {
-                        let updated_point = point.update(y_len as u32, x_len as u32, direction);
-                        if let Some(updated_point) = updated_point {
-                            queue.push_back((updated_point, direction));
-                        }
+                    } else if let Some(updated_point) = point.update(y_len as u8, x_len as u8, direction) {
+                        queue.push_back((updated_point, direction));
                     }
                 },
-                '-' => {
+                b'-' => {
                     if direction == Direction::North || direction == Direction::South {
-                        let updated_point = point.update(y_len as u32, x_len as u32, Direction::East);
-                        if let Some(updated_point) = updated_point {
+                        if let Some(updated_point) = point.update(y_len as u8, x_len as u8, Direction::East) {
                             queue.push_back((updated_point, Direction::East));
                         }
-                        let updated_point = point.update(y_len as u32, x_len as u32, Direction::West);
-                        if let Some(updated_point) = updated_point {
+                        if let Some(updated_point) = point.update(y_len as u8, x_len as u8, Direction::West) {
                             queue.push_back((updated_point, Direction::West));
                         }
-                    } else {
-                        let updated_point = point.update(y_len as u32, x_len as u32, direction);
-                        if let Some(updated_point) = updated_point {
-                            queue.push_back((updated_point, direction));
-                        }
+                    } else if let Some(updated_point) = point.update(y_len as u8, x_len as u8, direction) {
+                        queue.push_back((updated_point, direction));
                     }
                 },
-                '.' => {
-                    let updated_point = point.update(y_len as u32, x_len as u32, direction);
-                    if let Some(updated_point) = updated_point {
+                b'.' => {
+                    if let Some(updated_point) = point.update(y_len as u8, x_len as u8, direction) {
                         queue.push_back((updated_point, direction));
                     }
                 },
@@ -128,40 +116,25 @@ fn solve(grid: &[Vec<char>], start: Point, direction: Direction, size: (usize, u
             }
         }
     }
-    // Print the grid
     let set: FxHashSet<&Point> = visited.iter().map(|(point, _)| point).collect();
-    // let mut string = "".to_string();
-    // for (y, row) in grid.iter().enumerate() {
-    //     for (x, char) in row.iter().enumerate() {
-    //         let point = Point::new(y as u32, x as u32);
-    //         if set.contains(&point) {
-    //             string.push('#');
-    //         } else {
-    //             string.push('.');
-    //         }
-    //     }
-    //     string.push('\n');
-    // }
-    // println!("{string}");
-
     set.len()
 }
 
 #[aoc(day16, part1)]
-fn part1(input: &(Vec<Vec<char>>, (usize, usize))) -> usize {
-    let (grid, size) = input;
-    solve(grid, Point::new(0, 0), Direction::East, *size)
+fn part1(input: &str) -> usize {
+    let (grid, size) = parse(input);
+    solve(&grid, Point::new(0, 0), Direction::East, size)
 }
 
 #[aoc(day16, part2)]
-fn part2(input: &(Vec<Vec<char>>, (usize, usize))) -> usize {
-    let (grid, size) = input;
+fn part2(input: &str) -> usize {
+    let (grid, size) = parse(input);
     let (y_len, x_len) = size;
 
-    let max1 = (0..*y_len).into_par_iter().map(|y| solve(grid, Point::new(y as u32, 0), Direction::East, *size)).max().unwrap();
-    let max2 = (0..*y_len).map(|y| solve(grid, Point::new(y as u32, *x_len as u32 - 1), Direction::West, *size)).max().unwrap();
-    let max3 = (0..*x_len).map(|x| solve(grid, Point::new(0, x as u32), Direction::South, *size)).max().unwrap();
-    let max4 = (0..*x_len).map(|x| solve(grid, Point::new(*y_len as u32 - 1, x as u32), Direction::North, *size)).max().unwrap();
+    let max1 = (0..y_len).into_par_iter().map(|y| solve(&grid, Point::new(y as u8, 0), Direction::East, size)).max().unwrap();
+    let max2 = (0..y_len).into_par_iter().map(|y| solve(&grid, Point::new(y as u8, x_len as u8 - 1), Direction::West, size)).max().unwrap();
+    let max3 = (0..x_len).into_par_iter().map(|x| solve(&grid, Point::new(0, x as u8), Direction::South, size)).max().unwrap();
+    let max4 = (0..x_len).into_par_iter().map(|x| solve(&grid, Point::new(y_len as u8 - 1, x as u8), Direction::North, size)).max().unwrap();
 
     max([max1, max2, max3, max4]).unwrap()
 }
@@ -184,11 +157,11 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        assert_eq!(part1(&parse(EXAMPLE)), 46);
+        assert_eq!(part1(EXAMPLE), 46);
     }
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse(EXAMPLE)), 51);
+        assert_eq!(part2(EXAMPLE), 51);
     }
 }
